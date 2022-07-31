57f7797448577994bf111b262a8cbcaefbdd383b
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-31 16:57:29 -0400

    Created required methods for Stack. Confirmed that all tests are passing.

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,34 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Stack:
     """
-    Put docstring here
+    Data structure that stores values in linked nodes. Uses last in first out (LIFO) to access the nodes in the stack.
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, val):
+        new_node = Node(val)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        old_top = self.top
+        self.top = old_top.next
+        return old_top.value
+
+    def peek(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+class Node:
+    def __init__(self, val, next_=None):
+        self.value = val
+        self.next = next_
