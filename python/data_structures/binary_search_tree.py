054e1d6ef1327b7ee828463eb43f692b20372359
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-07 20:31:26 -0400

    Created BinarySearchTree class with required functions. Checked tests individually to make sure everything works correctly. All tests are passing.

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -7,9 +7,45 @@ class BinarySearchTree(BinaryTree):
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, val):
+        node = Node(val)
+
+        if not self.root:
+            self.root = node
+            return
+
+        def walk(root, new_node):
+            if root is None:
+                return
+
+            if new_node.value < root.value:
+                if not root.left:
+                    root.left = new_node
+                else:
+                    walk(root.left, new_node)
+            else:
+                if not root.right:
+                    root.right = new_node
+                else:
+                    walk(root.right, new_node)
+
+        walk(self.root, node)
+
+
+    def contains(self, target):
+
+        def walk(root):
+            if root is None:
+                return False
+
+            if root.value == target:
+                return True
+
+            if target < root.value:
+                return walk(root.left)
+            else:
+                return walk(root.right)
+
+        return walk(self.root)
