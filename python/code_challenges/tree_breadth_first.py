149b32ae1c6cade3c098885ce5487587f2ba2ccb
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-08 18:34:21 -0400

    Created function breadth_first. Checked each test one at a time to make sure the function works correctly. All tests are passing.

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,22 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    values = []
+
+    if tree.root is None:
+        return values
+
+    breadth = Queue()
+    breadth.enqueue(tree.root)
+
+    while not breadth.is_empty():
+        root = breadth.dequeue()
+        values.append(root.value)
+        if root.left:
+            breadth.enqueue(root.left)
+        if root.right:
+            breadth.enqueue(root.right)
+
+    return values
