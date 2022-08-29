dd5d78cd95be50e47b08a4044e3d6f6c83f8b7f9
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-28 23:19:59 -0400

    Created unit tests for each method in the Hashtable class. Ran each test individually to verify that all methods are functioning properly. All tests are passing.

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -5,30 +5,73 @@ from data_structures.hashtable import Hashtable
 def test_exists():
     assert Hashtable
 
-
-@pytest.mark.skip("TODO")
-def test_get_apple():
-    hashtable = Hashtable()
-    hashtable.set("apple", "Used for apple sauce")
-    actual = hashtable.get("apple")
-    expected = "Used for apple sauce"
+# @pytest.mark.skip("TODO")
+def test_buckets_size():
+    table = Hashtable()
+    actual = len(table.buckets)
+    expected = 1024
     assert actual == expected
 
+# @pytest.mark.skip("TODO")
+def test_hash_in_range():
+    table = Hashtable(size=2)
+    keys = ["spam", "eggs", "bacon"]
+    for key in keys:
+        index = table.hash(key)
+        assert 0 <= index <= table.size
 
-@pytest.mark.skip("TODO")
-def test_internals():
-    hashtable = Hashtable(1024)
-    hashtable.set("ahmad", 30)
-    hashtable.set("silent", True)
-    hashtable.set("listen", "to me")
+# @pytest.mark.skip("TODO")
+def test_set_creates_new_entry():
+    table = Hashtable()
+    table.set("spam", "eggs")
+    assert True
 
-    actual = []
+# @pytest.mark.skip("TODO")
+def test_set_repeat_key():
+    table = Hashtable()
+    table.set("spam", "eggs")
+    table.set("spam", "bacon")
+    actual = table.get("spam")
+    expected = "bacon"
+    assert actual == expected
 
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable._buckets:
-        if item:
-            actual.append(item.display())
+# @pytest.mark.skip("TODO")
+def test_contains():
+    table = Hashtable()
+    table.set("spam", "eggs")
+    actual = table.contains("spam")
+    expected = True
+    assert actual == expected
 
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
+# @pytest.mark.skip("TODO")
+def test_not_contains():
+    table = Hashtable()
+    table.set("spam", "eggs")
+    actual = table.contains("bacon")
+    expected = False
+    assert actual == expected
 
+# @pytest.mark.skip("TODO")
+def test_keys():
+    table = Hashtable()
+    table.set("spam", "eggs")
+    table.set("eggs", "bacon")
+    keys = table.keys()
+    actual = sorted(keys)
+    expected = ["eggs", "spam"]
+    assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_get_returns_none_for_missing_key():
+    table = Hashtable()
+    actual = table.get("test")
+    expected = None
+    assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_get_returns_value_given_key():
+    table = Hashtable()
+    table.set("test", "Test value")
+    actual = table.get("test")
+    expected = "Test value"
     assert actual == expected
