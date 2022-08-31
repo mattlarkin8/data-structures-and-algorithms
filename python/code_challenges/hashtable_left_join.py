b566b45dffffd065787ed2be7ee158b0ebc281f3
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-31 17:28:34 -0400

    Created function left_join that performs the left join functionality. It returns the joined data in a 2D array. Created tests that verify the defining features of the left_join function. Ran through each test individually to make sure that everything is working correctly. All tests are passing.

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,16 @@
 from data_structures.hashtable import Hashtable
 
+def left_join(hash_a, hash_b):
+    hash_a_keys = hash_a.keys()
+    joined = []
 
-def left_join():
-    pass
+    for key in hash_a_keys:
+        item = [key, hash_a.get(key)]
+        right = hash_b.get(key)
+        if right is None:
+            item.append("NONE")
+        else:
+            item.append(right)
+        joined.append(item)
+
+    return joined
