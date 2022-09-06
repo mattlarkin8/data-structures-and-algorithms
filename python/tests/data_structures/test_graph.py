6cb7a5dd472b960f81805b8eeb953319bf06d3c9
Matthew Larkin <matthewlarkin8@gmail.com>
2022-09-05 21:09:12 -0400

    Created Graph class with all required methods. Ran each test individaully to make sure everything is functioning properly. All tests are passing.

--- python/tests/data_structures/test_graph.py
+++ python/tests/data_structures/test_graph.py
@@ -6,45 +6,33 @@ def test_exists():
     assert Graph
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_node():
-
     graph = Graph()
-
     expected = "spam"  # a vertex's value that comes back
-
     actual = graph.add_node("spam").value
-
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_size_empty():
-
     graph = Graph()
-
     expected = 0
-
     actual = graph.size()
-
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_size():
-
     graph = Graph()
-
     graph.add_node("spam")
 
     expected = 1
-
     actual = graph.size()
-
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_edge():
     g = Graph()
     apple = g.add_node("apple")
@@ -56,7 +44,7 @@ def test_add_edge():
     assert neighbors[0].weight == 5
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_bouquet():
     g = Graph()
     apple = g.add_node("apple")
@@ -67,61 +55,44 @@ def test_bouquet():
     assert neighbors[0].weight == 10
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_edge_interloper_start():
-
     graph = Graph()
-
     start = Vertex("start")
-
     end = graph.add_node("end")
 
     with pytest.raises(KeyError):
         graph.add_edge(start, end)
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_edge_interloper_end():
-
     graph = Graph()
-
     end = Vertex("end")
-
     start = graph.add_node("start")
 
     with pytest.raises(KeyError):
         graph.add_edge(start, end)
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_get_nodes():
-
     graph = Graph()
-
     banana = graph.add_node("banana")
-
     apple = graph.add_node("apple")
-
     loner = Vertex("loner")
 
     expected = 2
-
     actual = len(graph.get_nodes())
-
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_get_neighbors():
-
     graph = Graph()
-
     banana = graph.add_node("banana")
-
     apple = graph.add_node("apple")
-
     graph.add_edge(apple, banana, 44)
-
     neighbors = graph.get_neighbors(apple)
 
     assert len(neighbors) == 1
@@ -129,5 +100,4 @@ def test_get_neighbors():
     neighbor_edge = neighbors[0]
 
     assert neighbor_edge.vertex.value == "banana"
-
     assert neighbor_edge.weight == 44
