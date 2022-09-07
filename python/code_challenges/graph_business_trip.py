dc012435b9688808852566e7157570042e00f923
Matthew Larkin <matthewlarkin8@gmail.com>
2022-09-07 17:58:49 -0400

    Created direct_flights function with all required functionality. Created and tested different cases to ensure that the function returns proper values. All tests are passing.

--- python/code_challenges/graph_business_trip.py
+++ python/code_challenges/graph_business_trip.py
@@ -1,5 +1,27 @@
 from data_structures.graph import Graph
 
 
-def direct_flights():
-    pass
+def direct_flights(graph, names):
+    if len(names) == 0:
+        return "null"
+
+    nodes = {}
+    for vertex in graph.get_nodes():
+        nodes[vertex.value] = vertex
+
+    weight = 0
+    next_ = 1
+    for name in names:
+        if next_ < len(names):
+            links = {}
+            edges = graph.get_neighbors(nodes[name])
+            for edge in edges:
+                links[edge.vertex.value] = edge
+
+            if names[next_] not in links.keys():
+                return "null"
+
+            weight += links[names[next_]].weight
+            next_ += 1
+
+    return weight
